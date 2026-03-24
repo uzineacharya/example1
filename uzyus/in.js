a28298f1e29f12481e92e1127d9c7c3b1d4f92cc
U_acharya <uzineacharya55@gmail.com>
2026-03-24 12:28:54 +0545

    Add files via upload

---
+++ uzyus/in.js
@@ -0,0 +1,19 @@
+// Simple JavaScript Program
+
+// printing message
+console.log("Hello, World!");
+
+// variables
+let a = 10;
+let b = 5;
+
+// function
+function add(x, y) {
+    return x + y;
+}
+
+// calling function
+let result = add(a, b);
+
+// output
+console.log("Sum is:", result);
